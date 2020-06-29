f5d51067b7963596fc687b8870a3778bddadfd7d
黄彬 <huangbin@Yellow-2.local>
2020-06-30 07:24:29 +0800

    chapter2.1.3 write safety thread for local inference in try-exception case

--- chapter2.1.cpp
+++ chapter2.1.cpp
@@ -7,9 +7,33 @@ public:
         std::cout << "hello world!" << std::endl;
     }
 };
+
+struct func{
+    int & i;
+    func(int &_i):i(_i){}
+    void operator()(){
+        for(unsigned int j=0;j<10000;j++){
+            std:: cout << --i << std::endl;
+        }
+    }
+};
+
+class thread_guard{
+    std::thread& t;
+public:
+    explicit thread_guard(std::thread& _t): t(_t){}
+    ~thread_guard(){
+        if(t.joinable())
+        {
+            t.join();
+        }
+    }
+    thread_guard(thread_guard const&)=delete;
+    thread_guard& operator=(thread_guard const&)=delete;
+};
 int main(){
-    backgroudtask bg;
-    backgroudtask();
-    std::thread t((backgroudtask()));
-    t.join();
+    int local_stat = 100;
+    func bg(local_stat);
+    std::thread my_thread(bg);
+    thread_guard tg(my_thread);
 }
